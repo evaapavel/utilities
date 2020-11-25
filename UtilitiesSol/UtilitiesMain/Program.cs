eb877530c1a0122ead1aeb48a8dafc062bb4f168
Pavel Foltyn <pavel.foltyn@sssvt.cz>
2020-11-25 13:00:40 +0100

    CHANGE Add a new data class (Person).

--- UtilitiesSol/UtilitiesMain/Program.cs
+++ UtilitiesSol/UtilitiesMain/Program.cs
@@ -1,6 +1,7 @@
 using System;
-using UtilitiesMain.MethodExtraction.Example;
-using UtilitiesMain.MethodExtraction.Exercise;
+//using UtilitiesMain.MethodExtraction.Example;
+//using UtilitiesMain.MethodExtraction.Exercise;
+using UtilitiesMain.DataClasses;
 
 namespace UtilitiesMain
 {
@@ -10,29 +11,42 @@ namespace UtilitiesMain
         {
 
 
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            //Console.WriteLine("Hello World!");
-            //Console.ReadKey();
+            //// The code provided will print ‘Hello World’ to the console.
+            //// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            ////Console.WriteLine("Hello World!");
+            ////Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
 
 
-            // Try the "method extraction" example.
-            PersonService personService = new PersonService();
-            personService.MethodToRefactor();
-            Console.WriteLine();
-            Console.WriteLine();
-            personService.RefactoredMethod();
+            //// Try the "method extraction" example.
+            //PersonService personService = new PersonService();
+            //personService.MethodToRefactor();
+            //Console.WriteLine();
+            //Console.WriteLine();
+            //personService.RefactoredMethod();
 
-            // Merge files.
-            string pathToFileA = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileA.txt";
-            string pathToFileB = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileB.txt";
-            string pathToFileC = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileC.txt";
-            string pathToFileD = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileD.txt";
-            FileService fileService = new FileService();
-            fileService.MergeFiles(pathToFileA, pathToFileB, pathToFileC, pathToFileD);
+            //// Merge files.
+            //string pathToFileA = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileA.txt";
+            //string pathToFileB = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileB.txt";
+            //string pathToFileC = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileC.txt";
+            //string pathToFileD = @"C:\pfoltyn\MyProjects\Utilities\20201012_hodina\UtilitiesSol\Resources\InputFileD.txt";
+            //FileService fileService = new FileService();
+            //fileService.MergeFiles(pathToFileA, pathToFileB, pathToFileC, pathToFileD);
+
+
+            ShowDataClasses();
 
         }
+
+        static void ShowDataClasses()
+        {
+            Person pavel = new Person("Pavel", "Novák", new DateTime(1992, 9, 26), true);
+            Person jana = new Person("Jana", "Výborná", new DateTime(1987, 2, 4), false);
+            Console.WriteLine($"Jméno: {pavel.firstName}, Příjmení: {pavel.lastName}, Datum narození: {pavel.dateOfBirth}, Občan ČR: {pavel.isCitizenCZ}");
+            Console.WriteLine($"Jméno: {jana.firstName}, Příjmení: {jana.lastName}, Datum narození: {jana.dateOfBirth}, Občan ČR: {jana.isCitizenCZ}");
+            Console.ReadKey(true);
+        }
+
     }
 }
