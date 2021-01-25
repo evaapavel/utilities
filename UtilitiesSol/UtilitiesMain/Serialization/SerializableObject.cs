83a1638169c63e8930dbe41b311d919f2aca4d14
Pavel Foltyn <evaapavel@volny.cz>
2021-01-25 07:40:56 +0100

    CHANGE Start implementing SerializableObject.

--- UtilitiesSol/UtilitiesMain/Serialization/SerializableObject.cs
+++ UtilitiesSol/UtilitiesMain/Serialization/SerializableObject.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 
@@ -7,8 +7,54 @@ namespace UtilitiesMain.Serialization
 
 
 
-    public class SerializableObject
+    /// <summary>
+    /// Able to serialize and deserialize itself.
+    /// </summary>
+    public abstract class SerializableObject
     {
+        
+        
+        
+        private const int MaxFields = 100;
+        
+        
+        protected object[] fields;
+        protected int firstUnused;
+        
+        
+        
+        public SerializableObject()
+        {
+            this.fields = new object[MaxFields];
+            this.firstUnused = 0;
+        }
+        
+        
+        
+        protected abstract void StoreFieldsIntoArray();
+        
+        protected abstract void LoadFieldsFromArray();
+        
+        
+        
+        public void SerializeToFile(string pathToFile)
+        {
+            this.firstUnused = 0;
+            StoreFieldsIntoArray();
+            
+            // Serialize the contents of the array into the given file.
+            // ...
+        }
+        
+        
+        
+        public void DeserializeFromFile(string pathToFile)
+        {
+            
+        }
+        
+        
+        
     }
 
 
