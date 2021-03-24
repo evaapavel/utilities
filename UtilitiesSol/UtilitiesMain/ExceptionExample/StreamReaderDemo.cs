d99449b32eff2f456030a5178ad5197e43ef3e7a
Pavel Foltyn <foltyn@sssvt.cz>
2021-03-24 15:15:33 +0100

    CHANGE Add a "Structured Exception Handling" demo.

---
+++ UtilitiesSol/UtilitiesMain/ExceptionExample/StreamReaderDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilitiesMain.ExceptionExample
+{
+
+
+
+    public class StreamReaderDemo
+    {
+
+        public static void Go()
+        {
+            StreamReaderExceptionTest test = new StreamReaderExceptionTest();
+            test.TestStreamReader();
+        }
+
+    }
+
+
+
+}
---
+++ UtilitiesSol/UtilitiesMain/ExceptionExample/StreamReaderExceptionTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+
+namespace UtilitiesMain.ExceptionExample
+{
+
+
+
+    public class StreamReaderExceptionTest
+    {
+
+        public void TestStreamReader()
+        {
+            try
+            {
+                TestStreamReader();
+                //using (StreamReader reader = new StreamReader(@"C:\MyDir\MyFile.txt"))
+                //using (StreamReader reader = new StreamReader((string) null))
+                //using (StreamReader reader = new StreamReader(""))
+                using (StreamReader reader = new StreamReader(@"X:\MyDir\MyFile.txt"))
+                //using (StreamReader reader = new StreamReader(@"Y:\MyDir\MyFile.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (ArgumentNullException e1)
+            {
+                Console.WriteLine("Given path is null.");
+            }
+            catch (ArgumentException e2)
+            {
+                Console.WriteLine("Given path is an empty string.");
+            }
+            catch (FileNotFoundException e3)
+            {
+                Console.WriteLine("The file cannot be found.");
+            }
+            catch (DirectoryNotFoundException e4)
+            {
+                Console.WriteLine("The specified path is invalid (unmapped drive).");
+            }
+            catch (IOException e5)
+            {
+                Console.WriteLine("An input/output error occurs.");
+            }
+            catch (Exception e6)
+            {
+                Console.WriteLine($"Unknown error: {e6.Message}");
+            }
+        }
+
+    }
+
+
+
+}
